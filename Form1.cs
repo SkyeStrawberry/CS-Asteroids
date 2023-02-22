552f97860af6f27bedeab4ab9bbc9fb92a610401
skye-skyla <twitch_ff3nixx@outlook.com>
2023-02-22 15:40:26 +0000

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Enumeration;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms.VisualStyles;
+using System.Xml.Serialization;
+
+namespace Asteroids
+{
+	public partial class Form1 : Form
+	{
+		Player player;
+		public Form1()
+		{
+			InitializeComponent();
+
+			foreach (Control control in this.Controls)
+			{
+				if (!(control is PictureBox)) continue;
+				if (control.Tag.ToString() == "SpawnPoint.Player") this.player = new Player(new Point(control.Left, control.Top), Color.White);
+			}
+			if(this.player is null) this.player = new Player(new Point(0,0), Color.White);
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+
+		}
+
+		private void KeyIsDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Up) this.player.accelerating = 1;
+			if (e.KeyCode == Keys.Down) this.player.accelerating = -1;
+			if (e.KeyCode == Keys.Left) this.player.rotationDirection = -1;
+			if (e.KeyCode == Keys.Right) this.player.rotationDirection = 1;
+			if (e.KeyCode == Keys.Space) this.player.Shoot();
+		}
+
+		private void KeyIsUp(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Up) this.player.accelerating = 0;
+			if (e.KeyCode == Keys.Down) this.player.accelerating = 0;
+			if (e.KeyCode == Keys.Left) this.player.rotationDirection = 0;
+			if (e.KeyCode == Keys.Right) this.player.rotationDirection = 0;
+		}
+
+		private void GameTick(object sender, EventArgs e)
+		{
+			foreach (Projectile p in this.player.projectiles.ToList()) 
+			{
+				if (!p.InBounds(this)) this.player.projectiles.Remove(p);
+				p.Move();
+			}
+			this.player.Move().Rotate().Friction();
+			Refresh();
+		}
+
+		private void DrawFrame(object sender, PaintEventArgs e)
+		{
+			foreach (Projectile p in this.player.projectiles) p.Draw(e);
+			this.player.Draw(e);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+	
+	public class Projectile
+	{
+		public double angle;
+		public double speed;
+
+		public (double sin, double cos) sinCos;
+		
+		public int length;
+		
+		public Player parent;
+
+		public Point center;
+		public Point start;
+		public Point end;
+
+		public Color color;
+
+		public Projectile(Player parent)
+		{
+			this.angle = parent.angle;
+			this.speed = 10 + parent.velocity.Magnitude();
+			this.length = 10;
+			this.parent = parent;
+			this.center = parent.verticies[0];
+			this.color = parent.color;
+			this.sinCos = Math.SinCos(this.angle);
+
+			this.start = new Point(this.center.X - (this.sinCos.sin * this.length)/2, this.center.Y + (this.sinCos.cos * this.length)/2);
+			this.end = new Point(this.center.X + (this.sinCos.sin * this.length)/2, this.center.Y - (this.sinCos.cos * this.length)/2);
+		}
+
+		public Projectile Move()
+		{
+			Point changeVector = new Point(this.sinCos.sin * this.speed, -this.sinCos.cos * this.speed);
+
+			this.center += changeVector; 
+			this.start += changeVector;
+			this.end += changeVector;
+			return this;
+		}
+
+		public Projectile Draw(PaintEventArgs events)
+		{
+			Pen pen = new Pen(color);
+			events.Graphics.DrawLine(pen, (int)this.start.X, (int)this.start.Y, (int)this.end.X, (int)this.end.Y);
+			return this;
+		}
+
+		public bool InBounds(Asteroids.Form1 form)
+		{
+			if(this.start.X < 0 || form.Width < this.start.X || form.Width < this.start.Y || this.start.Y < 0) return false;
+			return true;
+		}
+	}
+
+	public class Point
+	{
+		public double X;
+		public double Y;
+
+		public Point(double x, double y) {
+			this.X = x;
+			this.Y = y;
+		}
+		public static Point operator +(Point left, Point right)
+		{
+			return new Point(left.X + right.X, left.Y + right.Y);	
+		}
+
+		public static Point operator -(Point left, Point right)
+		{
+			return new Point(left.X - right.X, left.Y - right.Y);
+		}
+
+		public override string ToString()
+		{
+			return $"{{X= {this.X}, Y= {this.Y}}}";
+		}
+
+		public static Point Sum(params Point[] points)
+		{
+			Point res = new Point(0,0);
+
+			foreach (Point p in points)
+			{
+				res += p;
+			}
+			
+			return res;
+		}
+
+		public double Magnitude()
+		{
+			return Math.Sqrt(Math.Pow(this.X, 2) + Math.Pow(this.Y, 2));
+		}
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Asteroids
+{
+	public class Player
+	{
+		public double angle = 0;
+		public double acceleration = 3;
+		public double frictionCoefficient = 0.07;
+		public double angularVelocity = 0;
+		public double angularAcceleration = 3 * Math.PI / 180;
+		public double angularFriction = 15 * Math.PI / 180;
+		public double shotTimer = 0.5;
+		public double lastShotTime;
+
+		public int shots = 0;
+		public int hits = 0;
+		public int score = 0;
+		public int rotationDirection = 0;
+		public int accelerating = 0;
+		public int sideLength = 24;
+
+		public bool invulnerable = false;
+
+		public Point center;
+		public Point velocity;
+		public Color color = Color.White;
+
+		public List<Point> verticies;
+		public List<Projectile> projectiles;
+
+		public Player(Point center, Color color)
+		{
+			this.center = center;
+			this.color = color;
+			this.velocity = new Point(0, 0);
+			this.projectiles = new List<Projectile> { };
+			this.verticies = new List<Point>
+			{
+				new Point(center.X, center.Y - (this.sideLength * 2 * Math.Sqrt(3))/6),
+				new Point(center.X - (this.sideLength / 2), center.Y + this.sideLength / 9 + this.sideLength * Math.Sqrt(3)/6),
+				new Point(center.X + (this.sideLength / 2), center.Y + this.sideLength / 9 + this.sideLength * Math.Sqrt(3)/6),
+			};
+		}
+
+		private Player __Move(Point velocity)
+		{
+			foreach (Point point in this.verticies.ToList())
+			{
+				this.verticies[this.verticies.IndexOf(point)] += velocity;
+			}
+			this.center += velocity;
+			return this;
+		}
+
+		public Player Move()
+		{
+			double deltaX = (this.accelerating * this.acceleration * Math.Sin(this.angle)) / 2;
+			double deltaY = (this.accelerating * this.acceleration * Math.Cos(this.angle)) / 2;
+			this.velocity = new Point(this.velocity.X + deltaX, this.velocity.Y - deltaY);
+			this.__Move(this.velocity);
+			return this;
+		}
+		public Player Draw(PaintEventArgs events)
+		{
+			Pen pen = new Pen(color);
+			List<Point> verticiesCopy = new List<Point> { this.verticies[0], this.verticies[1], this.center, this.verticies[2] };
+			foreach (Point point in verticiesCopy)
+			{
+				int nextIndex = verticiesCopy.IndexOf(point) + 1;
+				if (verticiesCopy.IndexOf(verticiesCopy.Last()) < nextIndex) nextIndex = 0;
+				events.Graphics.DrawLine(pen, (int)point.X, (int)point.Y, (int)verticiesCopy[nextIndex].X, (int)verticiesCopy[nextIndex].Y);
+			}
+
+			return this;
+		}
+
+		public Player Rotate()
+		{
+			/*
+			 * Calculates the new coordinates of each vertex using 
+			 * x' = ( x - xᵣ)cos(θ) - (y - yᵣ)sin(θ) + xᵣ
+			 * y' = ( x - xᵣ)sin(θ) + (y - yᵣ)cos(θ) + yᵣ , where:
+			 * (xᵣ, yᵣ) is the point of rotation, and
+			 * (x', y') is the new set of cordinates for the vertex, and
+			 * θ is the change in angle
+			 */
+
+			this.angularVelocity += this.rotationDirection * this.angularAcceleration;
+			this.angle += this.angularVelocity;
+
+			List<Point> verticiesCopy = new List<Point> { this.verticies[0], this.verticies[1], this.verticies[2] };
+			(double sin, double cos) sc = Math.SinCos(this.angularVelocity);
+
+			foreach (Point point in verticiesCopy)
+			{
+				/*x' = ( x - xᵣ)cos(θ) - (y - yᵣ)sin(θ) + xᵣ*/
+				double xPrime = (point.X - this.center.X) * sc.cos - (point.Y - this.center.Y) * sc.sin + this.center.X;
+				/*y' = ( x - xᵣ)sin(θ) + (y - yᵣ)cos(θ) + yᵣ*/
+				double yPrime = (point.X - this.center.X) * sc.sin + (point.Y - this.center.Y) * sc.cos + this.center.Y;
+				this.verticies[this.verticies.IndexOf(point)] = new Point(xPrime, yPrime);
+			}
+			return this;
+		}
+
+		public Player Friction()
+		{
+			double resolveX = this.velocity.X - (this.velocity.X * this.frictionCoefficient);
+			double resolveY = this.velocity.Y - (this.velocity.Y * this.frictionCoefficient);
+
+			this.velocity = new Point(resolveX, resolveY);
+			this.angularVelocity -= this.angularVelocity * this.angularFriction;
+
+			return this;
+		}
+
+		public Player Shoot()
+		{
+			this.projectiles.Add(new Projectile(this));
+			this.shots += 1;
+			return this;
+		}
+	}
+}
