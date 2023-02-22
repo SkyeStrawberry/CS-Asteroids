0a72ef42f161cd7491f56eb6f2863ae40a8e3429
skye-skyla <twitch_ff3nixx@outlook.com>
2023-02-22 15:58:17 +0000

    aaa

---
+++ Point.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Asteroids
+{
+	public class Point
+	{
+		public double X;
+		public double Y;
+
+		public Point(double x, double y)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+		public static Point operator +(Point left, Point right)
+		{
+			return new Point(left.X + right.X, left.Y + right.Y);
+		}
+
+		public static Point operator -(Point left, Point right)
+		{
+			return new Point(left.X - right.X, left.Y - right.Y);
+		}
+
+		public override string ToString()
+		{
+			return $"{{X= {this.X}, Y= {this.Y}}}";
+		}
+
+		public static Point Sum(params Point[] points)
+		{
+			Point res = new Point(0, 0);
+
+			foreach (Point p in points)
+			{
+				res += p;
+			}
+
+			return res;
+		}
+
+		public double Magnitude()
+		{
+			return Math.Sqrt(Math.Pow(this.X, 2) + Math.Pow(this.Y, 2));
+		}
+	}
+}
---
+++ Projectile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Asteroids
+{
+	public class Projectile
+	{
+		public double angle;
+		public double speed;
+
+		public (double sin, double cos) sinCos;
+
+		public int length;
+
+		public Player parent;
+
+		public Point center;
+		public Point start;
+		public Point end;
+
+		public Color color;
+
+		public Projectile(Player parent)
+		{
+			this.angle = parent.angle;
+			this.speed = 10 + parent.velocity.Magnitude();
+			this.length = 10;
+			this.parent = parent;
+			this.center = parent.verticies[0];
+			this.color = parent.color;
+			this.sinCos = Math.SinCos(this.angle);
+
+			this.start = new Point(this.center.X - (this.sinCos.sin * this.length) / 2, this.center.Y + (this.sinCos.cos * this.length) / 2);
+			this.end = new Point(this.center.X + (this.sinCos.sin * this.length) / 2, this.center.Y - (this.sinCos.cos * this.length) / 2);
+		}
+
+		public Projectile Move()
+		{
+			Point changeVector = new Point(this.sinCos.sin * this.speed, -this.sinCos.cos * this.speed);
+
+			this.center += changeVector;
+			this.start += changeVector;
+			this.end += changeVector;
+			return this;
+		}
+
+		public Projectile Draw(PaintEventArgs events)
+		{
+			Pen pen = new Pen(color);
+			events.Graphics.DrawLine(pen, (int)this.start.X, (int)this.start.Y, (int)this.end.X, (int)this.end.Y);
+			return this;
+		}
+
+		public bool InBounds(Asteroids.Form1 form)
+		{
+			if (this.start.X < 0 || form.Width < this.start.X || form.Width < this.start.Y || this.start.Y < 0) return false;
+			return true;
+		}
+	}
+}
